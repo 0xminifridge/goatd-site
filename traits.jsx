4b53afa029a6bdb598a1e97dc931dfbbaf1e1741
Zubin Madon <35947376+zubin-madon@users.noreply.github.com>
2022-02-18 11:30:55 +0530

    Create traits.jsx

---
+++ traits.jsx
@@ -0,0 +1,137 @@
+const traits = [
+    {
+      id: 1,
+      image: './GoatdTraits/1.png',
+      traitName: '',
+      traitType: '',
+      rarity: ''
+    },
+    {
+        id: 2,
+        image: './GoatdTraits/2.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 3,
+        image: './GoatdTraits/100.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 4,
+        image: './GoatdTraits/101.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 5,
+        image: './GoatdTraits/102.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 6,
+        image: './GoatdTraits/103.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 7,
+        image: './GoatdTraits/104.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 8,
+        image: './GoatdTraits/105.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 9,
+        image: './GoatdTraits/200.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 10,
+        image: './GoatdTraits/201.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 11,
+        image: './GoatdTraits/202.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 12,
+        image: './GoatdTraits/300.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 13,
+        image: './GoatdTraits/301.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 14,
+        image: './GoatdTraits/302.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 15,
+        image: './GoatdTraits/303.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 16,
+        image: './GoatdTraits/400.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 17,
+        image: './GoatdTraits/401.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 18,
+        image: './GoatdTraits/600.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+      {
+        id: 19,
+        image: './GoatdTraits/601.png',
+        traitName: '',
+        traitType: '',
+        rarity: ''
+      },
+
+]
+export default traits;
